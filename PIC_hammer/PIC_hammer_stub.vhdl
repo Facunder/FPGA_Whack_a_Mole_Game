af7d8f227160b1a085fc6729194d9ac59b6f30d6
Facunder <127582736+Facunder@users.noreply.github.com>
2024-11-18 10:01:41 +0800

    Add files via upload

---
+++ PIC_hammer/PIC_hammer_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Thu May 18 10:41:36 2023
+-- Host        : FaceWord running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Vivado
+--               Demo/Whack_A_Mole/Whack_A_Mole.srcs/sources_1/ip/PIC_hammer/PIC_hammer_stub.vhdl}
+-- Design      : PIC_hammer
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity PIC_hammer is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 11 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end PIC_hammer;
+
+architecture stub of PIC_hammer is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[11:0],douta[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1";
+begin
+end;
