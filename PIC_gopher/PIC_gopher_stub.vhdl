36bd41baee1c84d5e20ffa7f572bfd4f4e317ff4
Facunder <127582736+Facunder@users.noreply.github.com>
2024-11-18 10:01:07 +0800

    Add files via upload

---
+++ PIC_gopher/PIC_gopher_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Tue May  9 11:17:59 2023
+-- Host        : FaceWord running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Vivado
+--               Demo/Whack_A_Mole/Whack_A_Mole.srcs/sources_1/ip/PIC_gopher/PIC_gopher_stub.vhdl}
+-- Design      : PIC_gopher
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity PIC_gopher is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 11 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end PIC_gopher;
+
+architecture stub of PIC_gopher is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[11:0],douta[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1";
+begin
+end;
